8a09d9e26f9d740c2b17a7905e6fe01228d0cfb2
bakoliasn <bakoliasn@gmail.com>
2015-11-26 18:11:46 +0000

    actually exercise 3!!!!

--- boilerplate.js
+++ boilerplate.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var color = require('colors');
 
 var connection = mysql.createConnection({
   host     : process.env.IP,
@@ -6,19 +7,12 @@ var connection = mysql.createConnection({
   password : '',
   database : 'addressbook'
 });
+connection.query("SELECT email FROM Account LIMIT 5", function(err, rows) {
+    if(err) throw err;
+    for(var i = 0; i < rows.length; i++){
+    console.log(rows[i].email);
 
-connection.query("SELECT * FROM Account", function(err, rows, fields) {
-  // In this callback, rows will be all the rows of the query, in a regular array of regular objects
-  // fields is not used very often, but it will contain a listing of the columns with some metadata
-  
-  // Here is an example usage:
-  rows.forEach(function(row) {
-    console.log('#' + row.id + ': ' + row.email);
-  });
-  // This code will output lines like:
-  // #1: john@smith.com
-  // #2: abc@def.com
-  // #5: xx@yy.com
-  
-  // Note that IDs do not have to be contiguous. If we DELETE rows, there will be holes in the ID list. This is normal.
+      
+    }
+  connection.end();
 });
