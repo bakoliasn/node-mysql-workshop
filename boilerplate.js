b2423c020bfc55f2ed653edc570199ebac7ac939
bakoliasn <bakoliasn@gmail.com>
2015-11-26 23:24:51 +0000

    progress

--- boilerplate.js
+++ boilerplate.js
@@ -1,24 +1,54 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : process.env.IP,
-  user     : process.env.C9_USER,
-  password : '',
-  database : 'addressbook'
+  host: process.env.IP,
+  user: process.env.C9_USER,
+  password: '',
+  database: 'addressbook'
 });
 
-connection.query("SELECT * FROM Account", function(err, rows, fields) {
-  // In this callback, rows will be all the rows of the query, in a regular array of regular objects
-  // fields is not used very often, but it will contain a listing of the columns with some metadata
-  
-  // Here is an example usage:
-  rows.forEach(function(row) {
-    console.log('#' + row.id + ': ' + row.email);
-  });
-  // This code will output lines like:
-  // #1: john@smith.com
-  // #2: abc@def.com
-  // #5: xx@yy.com
-  
-  // Note that IDs do not have to be contiguous. If we DELETE rows, there will be holes in the ID list. This is normal.
+connection.query("SELECT Account.id AS actId, Account.email AS actEmail, AddressBook.id AS adrId, AddressBook.accountId AS adrActId, AddressBook.name AS adrName, Entry.id AS entId, Entry.addressbookId AS entAdrId, Entry.firstName AS first, Entry.lastName AS last FROM Account JOIN AddressBook ON Account.id=AddressBook.accountId JOIN Entry ON Entry.addressbookId=AddressBook.id", function(err, rows) {
+  if (err) throw err;
+
+  var array = [];
+  var objAct = {
+    addressBooks: []
+  };
+  var objAdrBook = {
+    entries: []
+  };
+
+  for (var i = 0; i < rows.length; i++) {
+    
+    var push
+    
+    var foundAct = false;
+    var currentAct;
+    var foundAdr = false;
+    var currentAdr;
+    for (var j = 0; j < rows.length; j++) {
+      if (rows[i].actId === array[j].actId) {
+        foundAct = true;
+        currentAct = j;
+      }
+      for (var k = 0; k < array[j].addressBooks.length; k++) {
+        if (rows[i].adrId === array[j].addressBooks[k]) {
+          foundAdr = true;
+          currentAdr = k;
+        }
+      }
+    }
+    if(!foundAct){
+      objAct.actId = rows[i].actId;
+      objAct.actEmail = rows[i].actEmail;
+      array.push(objAct)
+      currentAct = array.length -1;
+    }
+    if(!foundAdr){
+      objAdrBook.adrId = rows[i].adrId;
+      objAdrBook.
+    }
+    
+    
+  }
 });
