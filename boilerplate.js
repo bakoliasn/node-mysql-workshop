c4396b0263aa06ecd770e8fd928dda047b528e30
bakoliasn <bakoliasn@gmail.com>
2015-11-27 08:26:48 +0000

    if statement trouble checking for duplicate 'addressBookIds's

--- boilerplate.js
+++ boilerplate.js
@@ -10,45 +10,65 @@ var connection = mysql.createConnection({
 connection.query("SELECT Account.id AS actId, Account.email AS actEmail, AddressBook.id AS adrId, AddressBook.accountId AS adrActId, AddressBook.name AS adrName, Entry.id AS entId, Entry.addressbookId AS entAdrId, Entry.firstName AS first, Entry.lastName AS last FROM Account JOIN AddressBook ON Account.id=AddressBook.accountId JOIN Entry ON Entry.addressbookId=AddressBook.id", function(err, rows) {
   if (err) throw err;
 
+
   var array = [];
-  var objAct = {
-    addressBooks: []
-  };
-  var objAdrBook = {
-    entries: []
-  };
+
 
   for (var i = 0; i < rows.length; i++) {
-    
-    var push
-    
+
+    var objAct = {
+      addressBooks: []
+    };
+    var objAdrBook = {
+      entries: []
+    };
+
+
     var foundAct = false;
     var currentAct;
     var foundAdr = false;
     var currentAdr;
-    for (var j = 0; j < rows.length; j++) {
-      if (rows[i].actId === array[j].actId) {
-        foundAct = true;
-        currentAct = j;
+
+    for (var j = 0; j < array.length; j++) {
+      if (array[0] !== null) {
+        if (rows[i].actId === array[j].actId) {
+          foundAct = true;
+          currentAct = j;
+        }
       }
-      for (var k = 0; k < array[j].addressBooks.length; k++) {
-        if (rows[i].adrId === array[j].addressBooks[k]) {
+    }
+    if (!foundAct) {
+      objAct.actId = rows[i].actId;
+      objAct.actEmail = rows[i].actEmail;
+      array.push(objAct);
+      currentAct = array.length - 1;
+    }
+
+    for (var k = 0; k < array[currentAct].addressBooks.length; k++) {
+      if (array[currentAct].addressBooks[0] !== null) {
+        if (rows[i].adrId === array[currentAct].addressBooks[k] && rows[i].actId === array[j].actId) {
           foundAdr = true;
           currentAdr = k;
         }
       }
     }
-    if(!foundAct){
-      objAct.actId = rows[i].actId;
-      objAct.actEmail = rows[i].actEmail;
-      array.push(objAct)
-      currentAct = array.length -1;
-    }
-    if(!foundAdr){
+
+  
+    if (!foundAdr) {
       objAdrBook.adrId = rows[i].adrId;
-      objAdrBook.
+      objAdrBook.adrName = rows[i].adrName;
+      array[currentAct].addressBooks.push(objAdrBook);
+      currentAdr = array[currentAct].addressBooks.length - 1;
     }
-    
-    
+    array[currentAct].addressBooks[currentAdr].entries.push({
+      id: rows[i].entId,
+      firstName: rows[i].firstName,
+      lastName: rows[i].lastName
+    });
+
   }
+  for (var x = 0; x < array.length; x++){
+  console.log(array[i].addressBook);
+  }
+  connection.end();
 });
---
+++ test.js
@@ -0,0 +1,68 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: process.env.IP,
+    user: process.env.C9_USER,
+    password: '',
+    database: 'addressbook'
+});
+
+connection.query("SELECT Account.id AS actId, Account.email AS actEmail, AddressBook.id AS adrId, AddressBook.accountId AS adrActId, AddressBook.name AS adrName, Entry.id AS entId, Entry.addressbookId AS entAdrId, Entry.firstName AS first, Entry.lastName AS last FROM Account JOIN AddressBook ON Account.id=AddressBook.accountId JOIN Entry ON Entry.addressbookId=AddressBook.id", function(err, rows) {
+    if (err) throw err;
+
+    var array = [];
+
+
+
+    for (var i = 0; i < rows.length; i++) {
+        var objAct = {
+            addressBooks: []
+        };
+        var objAdr = {
+            entries: []
+        };
+        var currentAct;
+        var currentAdr;
+        var foundAct = false;
+        var foundAdr = false;
+
+
+        for (var j = 0; j < array.length; j++) {
+            if (array.length > 0) {
+                if (rows[i].actId === array[j].actId) {
+                    currentAct = j;
+                    foundAct = true;
+                }
+            }
+        }
+
+        if (!foundAct) {
+            objAct.actId = rows[i].actId;
+            objAct.actEmail = rows[i].actEmail;
+            array.push(objAct);
+            currentAct = array.length - 1;
+        }
+        
+        for(var count = 0; count < array[currentAct].addressBooks.length; count++){
+            //if statment not working!!!!!!! always returning false
+            if(array[currentAct].addressBooks[count].adrId===rows[i].adrId){
+            console.log(array[currentAct].addressBooks[count]);  
+            foundAdr = true;
+            currentAdr = array[currentAct].addressBooks.length-1;
+            }   
+        }
+        //console.log(foundAdr);
+        if (!foundAdr) {
+            objAdr.adrId = rows[i].adrId;
+            objAdr.adrName = rows[i].adrName;
+            array[currentAct].addressBooks.push(objAdr);
+            currentAdr = array[currentAct].addressBooks.length - 1;
+        }
+
+        //console.log("ARRAY    " + array[currentAct].addressBooks[currentAdr].adrId);
+        //console.log("ROW    " + rows[i].adrId);
+    }
+    //console.log(array[2]);
+    //   console.log(array[3].addressBooks);
+    connection.end();
+});
